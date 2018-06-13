b04eafc9882e1565a475ae520c8f938347aab3c1
Haku-Dell <g@kohaku.cc>
2018-06-13 17:30:49 +0800

    start up

---
+++ Identity/Infrustructure/AppIdentityDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Identity.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Identity.Infrustructure
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext() : base("IdentityDB") { }
+
+        static AppIdentityDbContext()
+        {
+            Database.SetInitializer(new IdentityDbInit());
+        }
+
+        public static AppIdentityDbContext Create()
+        {
+            return new AppIdentityDbContext();
+        }
+    }
+
+    public class IdentityDbInit
+            : DropCreateDatabaseIfModelChanges<AppIdentityDbContext>
+    {
+
+        protected override void Seed(AppIdentityDbContext context)
+        {
+            PerformInitialSetup(context);
+            base.Seed(context);
+        }
+
+        public void PerformInitialSetup(AppIdentityDbContext context)
+        {
+            // initial configuration will go here
+            // 初始化配置将放在这儿
+        }
+    }
+}
