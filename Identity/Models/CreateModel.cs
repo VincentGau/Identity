b6a027321590f8b37e299c37413dc088be6352a3
Haku-Home <gmkohaku@hotmail.com>
2018-06-13 23:28:18 +0800

    user authentication

--- Identity/Models/CreateModel.cs
+++ Identity/Models/CreateModel.cs
@@ -15,4 +15,12 @@ namespace Identity.Models
         [Required]
         public string Password { get; set; }
     }
+
+    public class LoginModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
 }
