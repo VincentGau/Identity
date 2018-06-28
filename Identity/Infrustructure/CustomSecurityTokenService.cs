58c3f2ed285aebf21c927c8b11adee320b3cf62d
Haku-Dell <g@kohaku.cc>
2018-06-28 17:32:10 +0800

    CustomSecurityTokenService

---
+++ Identity/Infrustructure/CustomSecurityTokenService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel;
+using System.IdentityModel.Configuration;
+using System.IdentityModel.Protocols.WSTrust;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+
+namespace Identity.Infrustructure
+{
+    public class CustomSecurityTokenService : SecurityTokenService
+    {
+
+        //static readonly string[] _addressExpected = { };
+        static readonly string _addressExpected = "http://localhost";
+
+        public CustomSecurityTokenService(SecurityTokenServiceConfiguration configuration)
+            : base(configuration)
+        {
+
+        }
+
+        protected override Scope GetScope(ClaimsPrincipal principal, RequestSecurityToken request)
+        {
+            var scope = new Scope(request.AppliesTo.Uri.OriginalString, SecurityTokenServiceConfiguration.SigningCredentials);
+            return scope;
+        }
+
+        protected override ClaimsIdentity GetOutputClaimsIdentity(ClaimsPrincipal principal, RequestSecurityToken request, Scope scope)
+        {
+            ClaimsIdentity outgoingIdentity = new ClaimsIdentity();
+            outgoingIdentity.AddClaims(principal.Claims);
+
+            return outgoingIdentity;
+        }
+
+        void ValidateAppliesTo(EndpointReference appliesTo)
+        {
+            if (appliesTo == null)
+            {
+                throw new InvalidRequestException("The appliesTo is null.");
+            }
+
+            if (!appliesTo.Uri.Equals(new Uri(_addressExpected)))
+            {
+                throw new InvalidRequestException(String.Format("The relying party address is not valid. Expected value is {0}, the actual value is {1}.", _addressExpected, appliesTo.Uri.AbsoluteUri));
+            }
+        }
+    }
+}
---
+++ Identity/Infrustructure/CustomSecurityTokenServiceConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Identity.Infrustructure
+{
+    public class CustomSecurityTokenServiceConfiguration : SecurityTokenServiceConfiguration
+    {
+    }
+}
